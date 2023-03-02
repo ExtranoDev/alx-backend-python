e1747bf32199fed528828574746580ca24da15b7
ExtranoDev <jeomade19@gmail.com>
2023-03-02 12:27:15 +0000

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Parameterize and patch as decorators"""
 import unittest
 from client import GithubOrgClient
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 
@@ -17,3 +17,16 @@ class TestGithubOrgClient(unittest.TestCase):
         test_org = test_client.org
         self.assertEqual(test_org, mock_get.return_value)
         mock_get.assert_called_once
+
+    def test_public_repos_url(self):
+        """Test that the result of _public_repos_url
+        is the expected one based on the mocked payload"""
+        return_val = {'repos_url':
+                      'https://api.github.com/orgs/google/repos'}
+        with patch('test_client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_public_repo:
+            test_client = GithubOrgClient('google')
+            mock_public_repo.return_value = return_val
+            test_return = test_client._public_repos_url
+            self.assertEqual(test_return,
+                             mock_public_repo.return_value.get('repos_url'))
