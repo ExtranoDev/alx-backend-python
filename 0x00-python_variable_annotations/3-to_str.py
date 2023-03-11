afc2aa6ed052e6b8c69b35f518a2704f757da52f
ExtranoDev <jeomade19@gmail.com>
2023-03-11 02:13:24 +0000

    Basic annotations to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """args:
+        n: float
+      return: stringed number
+    """
+    return str(n)
