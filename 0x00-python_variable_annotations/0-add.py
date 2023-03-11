8306c0639c88286a55271002d4ed196f9f26632e
ExtranoDev <jeomade19@gmail.com>
2023-03-11 02:04:16 +0000

    using annotations to solve problems

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Basic annotations - add"""
+
+
+def add(a: float, b: float) -> float:
+    """takes a float a and b as arguments
+    returns:
+        sum as a float
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor"""
+from math import floor
+
+
+def floor(n: float) -> int:
+    """args:
+        n: float
+      return: floored float
+    """
+    return floor(n)
