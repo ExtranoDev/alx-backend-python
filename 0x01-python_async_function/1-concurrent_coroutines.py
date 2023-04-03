c3529916b2fd97ac73d80a5028388c7f3c2b7490
ExtranoDev <jeomade19@gmail.com>
2023-04-03 23:01:12 +0000

    Variable annotation added

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """execute multiple coroutines same time with async"""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """multiple coroutines"""
     delays = []
     for i in range(n):
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Handles Tasks"""
-import asyncio
+import asyncioi
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Calls task_wait_random to check task random time"""
     delays = []
     for i in range(n):
